75bb141137f1508c914830d07f5ba11ae26f2113
Rutuja9696 <rutuja.patil@contentstact.com>
2021-03-12 18:27:15 +0530

    added function to update contact

--- controllers/actionController.js
+++ controllers/actionController.js
@@ -9,7 +9,13 @@ const sendResponse = require("../helpers/sendResponse");
 
 //verifying post request
 const verifyPostRequest = (req, res, next) => {
-  const requireProperties = ["firstName", "lastName"];
+  const requireProperties = [
+    "firstName",
+    "lastName",
+    "email",
+    "phone",
+    "status",
+  ];
   let result = requireProperties.every((key) => {
     return req.body[key];
   });
@@ -27,7 +33,6 @@ const verifyPostRequest = (req, res, next) => {
 const getAllContacts = (req, res, next) => {
   Contacts.find({})
     .then((allContacts) => {
-      console.log("All contacts");
       sendResponse(200, "Successful", allContacts, req, res);
     })
     .catch((err) => {
@@ -41,16 +46,17 @@ const getAllContacts = (req, res, next) => {
 };
 //add new contact
 const createContact = (req, res, next) => {
-  console.log(req.body);
-  const { firstName, lastName } = req.body;
+  const { firstName, lastName, email, phone, status } = req.body;
   let newContact = new Contacts({
     firstName,
     lastName,
+    email,
+    phone,
+    status,
   });
   newContact
     .save()
     .then((data) => {
-      console.log(data);
       sendResponse(201, "Successful", data, req, res);
     })
     .catch((err) => {
@@ -63,32 +69,40 @@ const createContact = (req, res, next) => {
     });
 };
 //delete contact
-// const deleteContact = (req, res, next) => {
-//   // console.log(req.params.id);
-//   Contact.findOneAndDelete({ contactId: req.params.id })
-//     .then((data) => {
-//       console.log(data);
-//       sendResponse(201, "Successful", data, req, res);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       sendErrorMessage(
-//         new AppError(400, "unsuccessful", "Id to delete not found"),
-//         req,
-//         res
-//       );
-//     });
-// };
 const deleteContactById = async (req, res) => {
-  const { contactId } = req.params.id;
+  const { contactId } = req.params;
   try {
-    let deleteContact = await Contacts.deleteOne({ contactId });
+    let deleteContact = await Contacts.deleteMany({ contactId });
     sendResponse(200, "Contact Deleted Successfully", deleteContact, req, res);
   } catch (err) {
     sendError(400, "Contact can't be deleted...", err, req, res);
   }
 };
+//update contact
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  try {
+    let update = {};
+    if (req.body.firstName) update.firstName = req.body.firstName;
+    if (req.body.lastName) update.lastName = req.body.lastName;
+    if (req.body.email) update.email = req.body.email;
+    if (req.body.phone) update.phone = req.body.phone;
+    if (req.body.status) update.status = req.body.status;
+
+    let contact = await Contacts.updateOne(
+      { contactId },
+      {
+        $set: update,
+      }
+    );
+    sendResponse(200, "Updated Successful", contact, req, res);
+  } catch (err) {
+    sendError(400, "Contact can't be updated by given id", err, req, res);
+  }
+};
+
 module.exports.verifyPostRequest = verifyPostRequest;
 module.exports.getAllContacts = getAllContacts;
 module.exports.createContact = createContact;
 module.exports.deleteContactById = deleteContactById;
+module.exports.updateContact = updateContact;
