42dce69985d8b73444d37b31c132fbc7e623356a
Rutuja9696 <rutuja.patil@contentstact.com>
2021-03-12 16:50:10 +0530

    added route to add new contact

--- routes/ContactsRoutes.js
+++ routes/ContactsRoutes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const {
   verifyPostRequest,
   getAllContacts,
+  createContact,
 } = require("../controllers/actionController");
 
 const router = express.Router();
 
-router.route("/").get(getAllContacts).post(verifyPostRequest);
+router.route("/").get(getAllContacts).post(verifyPostRequest, createContact);
+// router.route("/add").post(createContact);
+
 module.exports = router;
