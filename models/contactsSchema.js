eafca392830d40db6359e06b9dbac2dd0afe35b0
Rutuja9696 <rutuja.patil@contentstact.com>
2021-03-12 18:32:48 +0530

    updated schema

--- models/contactsSchema.js
+++ models/contactsSchema.js
@@ -1,31 +1,87 @@
 const mongoose = require("mongoose");
+const uniquid = require("uniquid");
+
 const contactsSchema = new mongoose.Schema({
   contactId: {
     type: String,
+    default: uniquid() + Date.now(),
   },
   firstName: {
     type: String,
-    required: [true, "Please enter your first name"],
-    validate: {
-      validator: function (firstName) {
-        console.log("this is validator", this);
-        return this.firstName.trim().length;
-        return true;
+    required: [true, "Please enter first name"],
+    validate: [
+      {
+        validator: function () {
+          return this.firstName.trim().length;
+        },
+        message: "Employee's First name should not be empty",
       },
-      message: "first name should not be empty",
-    },
+      {
+        validator: function () {
+          const re = /<("[^"]?"|'[^']?'|[^'">])*>/;
+          if (re.test(this.firstName)) {
+            return false;
+          }
+        },
+        message: "Field content cannot be HTML",
+      },
+    ],
   },
   lastName: {
     type: String,
     required: [true, "Please enter last name"],
-    validate: {
-      validator: function (lastName) {
-        console.log("this is validator", this);
-        return this.lastName.trim().length;
-        return true;
+    validate: [
+      {
+        validator: function () {
+          return this.lastName.trim().length;
+        },
+        message: "Employee's Last name should not be empty",
       },
-      message: "last name should not be empty",
-    },
+      {
+        validator: function () {
+          const re = /<("[^"]?"|'[^']?'|[^'">])*>/;
+          if (re.test(this.lastName)) {
+            return false;
+          }
+        },
+        message: "Field content cannot be HTML",
+      },
+    ],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email. "],
+    unique: [true, "Email Id already exist. Try with another Email Id. "],
+    validate: [
+      {
+        validator: function () {
+          return this.email.trim().length;
+        },
+        message: "Employee's Email should not be empty",
+      },
+      {
+        validator: function () {
+          const re = /<("[^"]?"|'[^']?'|[^'">])*>/;
+          if (re.test(this.email)) {
+            return false;
+          }
+        },
+        message: "Field content cannot be HTML",
+      },
+      {
+        validator: function () {
+          const re = /<("[^"]?"|'[^']?'|[^'">])*>/;
+          if (re.test(this.email)) {
+            return false;
+          }
+        },
+        message: "Email id is not valid",
+      },
+    ],
+  },
+  phone: {
+    type: Number,
+    default: "",
   },
   status: {
     type: String,
